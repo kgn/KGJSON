5415d04a439d0b33c16df26b8665d91290d9a666
David Keegan <dkeegan@signnow.com>
2012-10-16 00:43:21 -0700

    initial commit

---
+++ KGJSON.h
@@ -0,0 +1,31 @@
+//
+//  NSString+KGJSON.h
+//  KGJSON
+//
+//  Created by David Keegan on 10/16/12.
+//  Copyright (c) 2012 David Keegan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString(KGJSON)
+
++ (id)JSONObject;
++ (NSString *)JSONString:(id)object;
++ (NSString *)prettyJSONString:(id)object;
+
+@end
+
+@interface NSDictionary(KGJSON)
+
+- (NSString *)JSONString;
+- (NSString *)prettyJSONString;
+
+@end
+
+@interface NSArray(KGJSON)
+
+- (NSString *)JSONString;
+- (NSString *)prettyJSONString;
+
+@end
---
+++ KGJSON.m
@@ -0,0 +1,52 @@
+//
+//  NSString+KGJSON.m
+//  KGJSON
+//
+//  Created by David Keegan on 10/16/12.
+//  Copyright (c) 2012 David Keegan. All rights reserved.
+//
+
+#import "KGJSON.h"
+
+@implementation NSString(KGJSON)
+
++ (id)JSONObject{
+    NSData *data = [(NSString *)self dataUsingEncoding:NSUTF8StringEncoding];
+    return [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+}
+
++ (NSString *)JSONString:(id)object{
+    NSData *json = [NSJSONSerialization dataWithJSONObject:object options:0 error:nil];
+    return [[NSString alloc] initWithData:json encoding:NSUTF8StringEncoding];
+}
+
++ (NSString *)prettyJSONString:(id)object{
+    NSData *json = [NSJSONSerialization dataWithJSONObject:object options:NSJSONWritingPrettyPrinted error:nil];
+    return [[NSString alloc] initWithData:json encoding:NSUTF8StringEncoding];
+}
+
+@end
+
+@implementation NSDictionary(KGJSON)
+
+- (NSString *)JSONString{
+    return [NSString JSONString:self];
+}
+
+- (NSString *)prettyJSONString{
+    return [NSString prettyJSONString:self];
+}
+
+@end
+
+@implementation NSArray(KGJSON)
+
+- (NSString *)JSONString{
+    return [NSString JSONString:self];
+}
+
+- (NSString *)prettyJSONString{
+    return [NSString prettyJSONString:self];
+}
+
+@end
